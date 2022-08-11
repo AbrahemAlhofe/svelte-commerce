36563527f87cdcfaf9c58d0d1e57d43fdbafe96a
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-08-12 00:11:17 +0200

    chore: use ( sveltekit ) config instead of ( vite ) config to set aliases

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			$stores: path.resolve("./src/stores")
-		}
-	}
+	plugins: [sveltekit()]
 };
 
 export default config;
