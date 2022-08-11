829117e6975b76a19a6a086279d9d97b911ec41b
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-08-11 23:07:03 +0200

    feat(components): link ( GridTile ) with ( products ) page

--- src/lib/grid/GridTile.svelte
+++ src/lib/grid/GridTile.svelte
@@ -5,21 +5,22 @@
     export let name: Product["name"] | undefined = undefined;
     export let description: Product["description"] | undefined = undefined;
     export let thumbnail: Product["thumbnail"] | undefined = undefined;
+    export let id: Product["id"] | undefined = undefined;
 
 </script>
 
 <div class="h-full w-full bg-indigo-600 overflow-hidden rounded">
-    <div class="relative flex h-full w-full items-center justify-center">
-      {#if thumbnail}
-        <img
-          src="{thumbnail}"
-          class="w-full md:w-1/2 lg:w-full flex-none transition duration-300 ease-in-out"
-          alt=""
-        />
-      {/if}
-      <div class="text-white absolute top-0 left-0 m-5">
-        {#if name} <div class="rounded bg-black p-3 text-2xl font-medium mb-3">{name}</div> {/if}
-        {#if description} <div class="rounded w-fit bg-black p-3 text-sm">{description}</div> {/if}
-      </div>
+  <svelte:element this={id === undefined ? "div" : "a"} href="/products/{id}" class="relative flex h-full w-full items-center justify-center">
+    {#if thumbnail}
+      <img
+        src="{thumbnail}"
+        class="w-full md:w-1/2 lg:w-full flex-none transition duration-300 ease-in-out"
+        alt=""
+      />
+    {/if}
+    <div class="text-white absolute top-0 left-0 m-5">
+      {#if name} <div class="rounded bg-black p-3 text-2xl font-medium mb-3">{name}</div> {/if}
+      {#if description} <div class="rounded w-fit bg-black p-3 text-sm">{description}</div> {/if}
     </div>
+  </svelte:element>
 </div>
