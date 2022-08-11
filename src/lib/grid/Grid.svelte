8515f7e02b00622321752b11750addc9e757848c
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-08-11 22:31:05 +0200

    refactor(components): divide GrideTile's product property into smaller properties

--- src/lib/grid/Grid.svelte
+++ src/lib/grid/Grid.svelte
@@ -9,14 +9,14 @@
 
 <div class="h-[50vh] w-full flex gap-2">
   <div class="w-2/3">
-    <GridTile bind:product={products[0]}/>
+    <GridTile {...products[0]}/>
   </div>
   <div class="w-1/3 flex flex-col gap-2">
     <div class="w-full h-1/2">
-      <GridTile bind:product={products[1]}/>
+      <GridTile {...products[1]}/>
     </div>
     <div class="w-full grow">
-      <GridTile bind:product={products[2]}/>
+      <GridTile {...products[2]}/>
     </div>
   </div>
 </div>
--- src/lib/grid/GridTile.svelte
+++ src/lib/grid/GridTile.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
 
-    import type { Product } from "./types";
+    import type { Product } from "$lib/types";
         
-    export let product: Product;
+    export let name: Product["name"] | undefined = undefined;
+    export let description: Product["description"] | undefined = undefined;
+    export let thumbnail: Product["thumbnail"] | undefined = undefined;
 
 </script>
 
 <div class="h-full w-full bg-indigo-600 overflow-hidden rounded">
     <div class="relative flex h-full w-full items-center justify-center">
-      <img
-        src="{product.thumbnail}"
-        class="w-full md:w-1/2 lg:w-full flex-none transition duration-300 ease-in-out"
-        alt=""
-      />
+      {#if thumbnail}
+        <img
+          src="{thumbnail}"
+          class="w-full md:w-1/2 lg:w-full flex-none transition duration-300 ease-in-out"
+          alt=""
+        />
+      {/if}
       <div class="text-white absolute top-0 left-0 m-5">
-        <div class="rounded bg-black p-3 text-2xl font-medium mb-3">{product.name}</div>
-        <div class="rounded w-fit bg-black p-3 text-sm">{product.description}</div>
+        {#if name} <div class="rounded bg-black p-3 text-2xl font-medium mb-3">{name}</div> {/if}
+        {#if description} <div class="rounded w-fit bg-black p-3 text-sm">{description}</div> {/if}
       </div>
     </div>
 </div>
