391a7d312c5c74e7ff648752b2021484db71bd8b
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-08-20 20:35:00 +0200

    refactor(stores): make ( shoppingCart ) empty

--- src/stores/shoppingCart.ts
+++ src/stores/shoppingCart.ts
@@ -1,17 +1,9 @@
 import { writable } from "svelte/store";
 import type { ShoppingCartItem } from '$lib/types';
-import { faker } from "@faker-js/faker";
 
 export const shoppingCart = (() => {
 
-    const { subscribe, set, update } = writable<ShoppingCartItem[]>(new Array(2).fill(null).map(() => ({
-        id: faker.datatype.uuid(),
-        name: faker.commerce.productName(),
-        description: faker.commerce.productDescription(),
-        thumbnail: faker.image.food(80, 80, true),
-        price: Number.parseInt(faker.commerce.price()),
-        quantity: 1
-    })));
+    const { subscribe, update } = writable<ShoppingCartItem[]>([]);
 
     return {
 
