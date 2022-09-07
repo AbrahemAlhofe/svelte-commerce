1b44b8982bde1db3f05d0b821b637e1f0f20ee8d
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-09-08 00:25:10 +0200

    feat(pages): display 500s errors on ( error ) page
    	- replace ( search ) icon with ( page ) icon in 400s errors

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,22 +1,52 @@
 <script>
-    import SearchIcon from '$lib/icons/SearchIcon.svelte';
-  </script>
+	import PageIcon from '$lib/icons/PageIcon.svelte';
+  import ServerIcon from '$lib/icons/ServerIcon.svelte';
+  import { page } from '$app/stores';
+  import { dev } from '$app/env';
+</script>
   
-  <div class="flex h-screen flex-col items-center justify-center ">
-    <div class="flex w-auto flex-col items-center justify-center p-6 md:p-0">
+
+{#if $page.status >= 500}
+
+  <div class="flex h-screen flex-col items-center justify-center">
+    <div class="flex w-auto flex-col items-center justify-center p-6 md:p-0 max-w-[100%]">
       <div class="mb-8 flex w-full items-center">
-        <div class="flex flex-none items-center justify-center rounded-full bg-white p-4">
-          <SearchIcon color="black" />
+        <div class="flex flex-none items-center justify-center rounded-md bg-white flex-col p-3 gap-2">
+          <div><ServerIcon /></div>
+          <div>{ $page.status }</div>
         </div>
         <div class="mx-6 h-20 w-px bg-white" />
-        <div class="text-lg font-medium">Opps! The page you're looking for doesn't exist.</div>
+        <div class="text-lg font-medium">Sorry! But there is an Internal Server Error</div>
       </div>
-      <a
-        href="/"
-        sveltekit:prefetch
-        class="w-full bg-white/90 p-3 text-center text-sm font-medium uppercase text-black hover:bg-white/100"
-      >
-        Return to Home Page
-      </a>
+      {#if dev}
+        <pre
+          href="/"
+          class="w-full p-3 text-sm font-medium uppercase text-black max-h-[35vh] overflow-auto"
+        >{ $page.error.stack }</pre>
+      {/if}
     </div>
   </div>
+
+  {:else if $page.status >= 400}
+
+    <div class="flex h-screen flex-col items-center justify-center">
+      <div class="flex w-auto flex-col items-center justify-center p-6 md:p-0 max-w-[100%]">
+        <div class="mb-8 flex w-full items-center">
+          <div class="flex flex-none items-center justify-center rounded-md bg-white flex-col p-3 gap-2">
+            <div><PageIcon /></div>
+            <div>{ $page.status }</div>
+          </div>
+          <div class="mx-6 h-20 w-px bg-white" />
+          <div class="text-lg font-medium">Opps! The page you're looking for doesn't exist.</div>
+        </div>
+        <a
+          href="/"
+          sveltekit:prefetch
+          class="w-full bg-white/90 p-3 text-center text-sm font-medium uppercase text-black hover:bg-white/100"
+        >
+          Return to Home Page
+        </a>
+      </div>
+    </div>
+
+{/if}
