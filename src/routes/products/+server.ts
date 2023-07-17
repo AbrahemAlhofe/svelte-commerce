cdbd63800807aac5e944c0e2a1615edf82cf0bfa
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2023-07-17 17:39:27 +0300

    feat(database): connect to database based on an enviroment variable

--- src/routes/products/+server.ts
+++ src/routes/products/+server.ts
@@ -1,8 +1,13 @@
 import type { Product } from '$lib/types';
 import { json } from '@sveltejs/kit';
 import * as edgedb from 'edgedb';
+import type { ConnectConfig } from 'edgedb/dist/conUtils';
+import { DATABASE_SERVER_URL, DATABASE_TLS_SECURITY } from '$env/static/private';
 
-const client = edgedb.createClient();
+const client = edgedb.createClient({
+    dsn: DATABASE_SERVER_URL,
+    tlsSecurity: DATABASE_TLS_SECURITY as ConnectConfig['tlsSecurity']
+});
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
